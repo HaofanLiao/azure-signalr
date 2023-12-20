90f0db7431fad990f30f26d9d9c289276a4a9493
Haofan Liao <haofanliao@microsoft.com>
2023-12-20 17:22:47 +0800

    Add MessagingDiagnosticScope for OpenTelemetry distributed tracing

---
+++ src/Common/Azure.Core/AppContextSwitchHelper.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+#nullable enable
+
+namespace Azure.Core
+{
+    /// <summary>
+    /// Helper for interacting with AppConfig settings and their related Environment variable settings.
+    /// Copied from https://github.com/Azure/azure-sdk-for-net/blob/fd271cdcb9fc8a888d917f7e0d43fd6d6f19fcb1/sdk/core/Azure.Core/src/Shared/AppContextSwitchHelper.cs
+    /// </summary>
+    internal static class AppContextSwitchHelper
+    {
+        /// <summary>
+        /// Determines if either an AppContext switch or its corresponding Environment Variable is set
+        /// </summary>
+        /// <param name="appContexSwitchName">Name of the AppContext switch.</param>
+        /// <param name="environmentVariableName">Name of the Environment variable.</param>
+        /// <returns>If the AppContext switch has been set, returns the value of the switch.
+        /// If the AppContext switch has not been set, returns the value of the environment variable.
+        /// False if neither is set.
+        /// </returns>
+        public static bool GetConfigValue(string appContexSwitchName, string environmentVariableName)
+        {
+            // First check for the AppContext switch, giving it priority over the environment variable.
+            if (AppContext.TryGetSwitch(appContexSwitchName, out bool value))
+            {
+                return value;
+            }
+            // AppContext switch wasn't used. Check the environment variable.
+            string? envVar = Environment.GetEnvironmentVariable(environmentVariableName);
+            if (envVar != null && (envVar.Equals("true", StringComparison.OrdinalIgnoreCase) || envVar.Equals("1")))
+            {
+                return true;
+            }
+
+            // Default to false.
+            return false;
+        }
+    }
+}
---
+++ src/Microsoft.Azure.SignalR.Common/Diagnostic/DiagnosticProperty.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Azure.SignalR.Diagnostic;
+
+internal class DiagnosticProperty
+{
+    /// <summary>The namespace used for the SignalR diagnostic scope.</summary>
+    public const string DiagnosticNamespace = "Microsoft.Azure.SignalR";
+    
+    /// <summary>The namespace used for the Azure Resource Manager provider namespace.</summary>
+    public const string ResourceProviderNamespace = "Microsoft.SignalR";
+    
+    /// <summary>The value which identifies the SignalR diagnostics context.</summary>
+    public const string SignalRServiceContext = "signalr";
+    
+}
